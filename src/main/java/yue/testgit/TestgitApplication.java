c0eb27d387f7cd773e407d19c0c0e8b3b533d25b
yue <842828087@qq.com>
2019-07-23 12:56:17 +0800

    test

---
+++ src/main/java/yue/testgit/TestgitApplication.java
@@ -0,0 +1,14 @@
+package yue.testgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestgitApplication {
+
+	public static void main(String[] args) {
+		System.out.println("yue!");
+		SpringApplication.run(TestgitApplication.class, args);
+	}
+
+}
